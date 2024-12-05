6a083c5d5d21fa312d70ca24b212afa47b678fe5
Grandpa EJ <fakeif352@gmail.com>
2024-12-05 09:30:17 +0600

    minor fixed _change_bio_graphql.py

--- fbapy/_apis/_graphql/_change_bio_graphql.py
+++ fbapy/_apis/_graphql/_change_bio_graphql.py
@@ -1,9 +1,13 @@
 from ..._utils import DefaultFuncs, parse_and_check_login
 import json
-
+import base64
 
 def change_bio_graphql(default_funcs: DefaultFuncs, ctx: dict):
     def change(bio_content: str, publish_bio: bool):
+        data = f"profile_tile_view:{ctx['user_id']}:intro:intro_bio:intro_card_bio:profile_timeline:1"
+        byte1 = data.encode('utf-8')
+        enc1 = base64.b64encode(byte1)
+        pvti = enc1.decode('utf-8')
         form = {
             "av": ctx["user_id"],
             "fb_api_caller_class": "RelayModern",
@@ -12,13 +16,14 @@ def change_bio_graphql(default_funcs: DefaultFuncs, ctx: dict):
             "variables": json.dumps(
                 {
                     "input": {
-                        "attribution_id_v2": "ProfileCometTimelineListViewRoot.react,comet.profile.timeline.list,unexpected,1703941286245,183952,190055527696468,,;ProfileCometTimelineListViewRoot.react,comet.profile.timeline.list,unexpected,1703940792344,513946,190055527696468,,;ProfileCometTimelineListViewRoot.react,comet.profile.timeline.list,via_cold_start,1703940775960,698443,190055527696468,,",
+                        "attribution_id_v2": "ProfileCometTimelineListViewRoot.react,comet.profile.timeline.list,unexpected,1733367271707,3800,190055527696468,,;CometHomeRoot.react,comet.home,via_cold_start,1733367247764,265762,4748854339,,",
                         "bio": bio_content,
                         "publish_bio_feed_story": publish_bio,
                         "actor_id": ctx["user_id"],
                         "client_mutation_id": "3",
                     },
                     "hasProfileTileViewID": True,
+                    "profileTileViewID":pvti,
                     "scale": 1.5,
                     "useDefaultActor": False,
                 }
